6584937930c2fa8cfb46a230506b90fb3e62d2bb
linlangliehu <l41951748@gmail.com>
2026-05-14 19:07:04 +0800

    fix: 手动推本地正确版 dist 覆盖远程
    CI 在 commit 701a248 之后的 bundle workflow 编译产出的
    dist/神秘复苏模拟器/界面/状态栏/index.html 只有 42 KB（缺少 App.vue 改动）,
    而本地 pnpm watch 正确编译出的是 594 KB（含 extractOptions/options-module/
    isFirstFloor 等全部新代码）。CI 编译失败原因待查；本提交先用本地编译产物
    覆盖远程，让 jsdelivr 能拉到正确状态栏代码。
    
    bundle.yaml 的 paths-ignore: dist/** 确保本提交不会触发 CI 再次构建。

--- dist/神秘复苏模拟器/脚本/MVU/index.js
+++ dist/神秘复苏模拟器/脚本/MVU/index.js
@@ -1,1 +1,45 @@
-import'https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js';
+import * as __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_artifact_bundle_js_63c1ca07__ from "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js";
+
+var __webpack_modules__ = {
+  "./src/神秘复苏模拟器/脚本/MVU/index.ts"(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+    eval('{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_artifact_bundle_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js */ "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js");\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMv56We56eY5aSN6IuP5qih5ouf5ZmoL+iEmuacrC9NVlUvaW5kZXgudHMiLCJtYXBwaW5ncyI6Ijs7QUFBMEYiLCJzb3VyY2VzIjpbInNyYzovL3RhdmVybl9oZWxwZXJfdGVtcGxhdGUvc3JjL+elnuenmOWkjeiLj+aooeaLn+WZqC/ohJrmnKwvTVZVL2luZGV4LnRzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCAnaHR0cHM6Ly90ZXN0aW5nY2YuanNkZWxpdnIubmV0L2doL01hZ2ljYWxBc3Ryb2d5L01hZ1ZhclVwZGF0ZS9hcnRpZmFjdC9idW5kbGUuanMnOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/神秘复苏模拟器/脚本/MVU/index.ts\n\n}');
+  },
+  "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate/artifact/bundle.js"(module) {
+    module.exports = __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_artifact_bundle_js_63c1ca07__;
+  }
+};
+
+var __webpack_module_cache__ = {};
+
+function __webpack_require__(moduleId) {
+  var cachedModule = __webpack_module_cache__[moduleId];
+  if (cachedModule !== undefined) {
+    return cachedModule.exports;
+  }
+  var module = __webpack_module_cache__[moduleId] = {
+    exports: {}
+  };
+  if (!(moduleId in __webpack_modules__)) {
+    delete __webpack_module_cache__[moduleId];
+    var e = new Error("Cannot find module '" + moduleId + "'");
+    e.code = "MODULE_NOT_FOUND";
+    throw e;
+  }
+  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  return module.exports;
+}
+
+(() => {
+  __webpack_require__.r = exports => {
+    if (typeof Symbol !== "undefined" && Symbol.toStringTag) {
+      Object.defineProperty(exports, Symbol.toStringTag, {
+        value: "Module"
+      });
+    }
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+  };
+})();
+
+var __webpack_exports__ = __webpack_require__("./src/神秘复苏模拟器/脚本/MVU/index.ts");
