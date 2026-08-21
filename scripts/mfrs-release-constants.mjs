5531f0073963316e89ffaf9b6c66f89602d9e693
linlangliehu <l41951748@gmail.com>
2026-08-21 15:27:28 +0800

    release(mfrs): v8.15.38 发布版 CDN_REF 更新 + 发布版 PNG 重建

--- scripts/mfrs-release-constants.mjs
+++ scripts/mfrs-release-constants.mjs
@@ -4,7 +4,7 @@
  */
 export const REPO = 'linlangliehu/tavern_helper_template';
 // v8.15.38: 沉浸模式 ST 抽屉 yield 修复（管理聊天文件等面板不再被 HUD 遮挡）
-export const CDN_REF = 'df8a73ea2a66873398627cf170cb0956007f74e2';
+export const CDN_REF = '635bf8a78a8a47a3fe72b2d73fc075b65a4824f7';
 export const CDN = `https://testingcf.jsdelivr.net/gh/${REPO}@${CDN_REF}/`;
 export const CDN_CACHE_VERSION = 'v81538_20260821_02';
 export const RELEASE_VERSION = '8.15.38';
