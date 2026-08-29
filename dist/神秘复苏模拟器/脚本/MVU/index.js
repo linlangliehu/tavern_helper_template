16062e48d201d9bb85b5389d13e5644f3891ec3a
linlangliehu <l41951748@gmail.com>
2026-08-29 12:42:44 +0800

    chore(mjsr): 神秘复苏卡 dist/MVU/数据库前端 与 src 同步更新

--- dist/神秘复苏模拟器/脚本/MVU/index.js
+++ dist/神秘复苏模拟器/脚本/MVU/index.js
@@ -1,1 +1,45 @@
-import'https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@0.171.0/artifact/bundle.js';
+import * as __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_0_171_0_artifact_bundle_js_e4ca0a15__ from "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@0.171.0/artifact/bundle.js";
+
+var __webpack_modules__ = {
+  "./src/神秘复苏模拟器/脚本/MVU/index.ts"(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+    eval('{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_0_171_0_artifact_bundle_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@0.171.0/artifact/bundle.js */ "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@0.171.0/artifact/bundle.js");\n// 运行时死代码：角色卡 mvu 脚本在 index.yaml 内联加载 MagVar，不走本入口。\n// 保留仅作源码对照；L1 pin 与 yaml 内联保持一致。\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMv56We56eY5aSN6IuP5qih5ouf5ZmoL+iEmuacrC9NVlUvaW5kZXgudHMiLCJtYXBwaW5ncyI6Ijs7QUFBQSxtREFBbUQ7QUFDbkQsaUNBQWlDO0FBQ2lFIiwic291cmNlcyI6WyJzcmM6Ly90YXZlcm5faGVscGVyX3RlbXBsYXRlL3NyYy/npZ7np5jlpI3oi4/mqKHmi5/lmagv6ISa5pysL01WVS9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyDov5DooYzml7bmrbvku6PnoIHvvJrop5LoibLljaEgbXZ1IOiEmuacrOWcqCBpbmRleC55YW1sIOWGheiBlOWKoOi9vSBNYWdWYXLvvIzkuI3otbDmnKzlhaXlj6PjgIJcbi8vIOS/neeVmeS7heS9nOa6kOeggeWvueeFp++8m0wxIHBpbiDkuI4geWFtbCDlhoXogZTkv53mjIHkuIDoh7TjgIJcbmltcG9ydCAnaHR0cHM6Ly90ZXN0aW5nY2YuanNkZWxpdnIubmV0L2doL01hZ2ljYWxBc3Ryb2d5L01hZ1ZhclVwZGF0ZUAwLjE3MS4wL2FydGlmYWN0L2J1bmRsZS5qcyc7XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/神秘复苏模拟器/脚本/MVU/index.ts\n\n}');
+  },
+  "https://testingcf.jsdelivr.net/gh/MagicalAstrogy/MagVarUpdate@0.171.0/artifact/bundle.js"(module) {
+    module.exports = __WEBPACK_EXTERNAL_MODULE_https_testingcf_jsdelivr_net_gh_MagicalAstrogy_MagVarUpdate_0_171_0_artifact_bundle_js_e4ca0a15__;
+  }
+};
+
+const __webpack_module_cache__ = {};
+
+function __webpack_require__(moduleId) {
+  const cachedModule = __webpack_module_cache__[moduleId];
+  if (cachedModule !== undefined) {
+    return cachedModule.exports;
+  }
+  const module = __webpack_module_cache__[moduleId] = {
+    exports: {}
+  };
+  if (!(moduleId in __webpack_modules__)) {
+    delete __webpack_module_cache__[moduleId];
+    const e = new Error("Cannot find module '" + moduleId + "'");
+    e.code = "MODULE_NOT_FOUND";
+    throw e;
+  }
+  __webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+  return module.exports;
+}
+
+(() => {
+  __webpack_require__.r = exports => {
+    if (Symbol.toStringTag) {
+      Object.defineProperty(exports, Symbol.toStringTag, {
+        value: "Module"
+      });
+    }
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+  };
+})();
+
+let __webpack_exports__ = __webpack_require__("./src/神秘复苏模拟器/脚本/MVU/index.ts");
